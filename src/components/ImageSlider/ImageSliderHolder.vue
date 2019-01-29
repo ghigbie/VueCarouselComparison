391e443b59cae375f97a1264021ff9c6ea709a9a
George Higbie <ghigbie@monigle.com>
2019-01-29 09:39:35 -0700

    Add image slider components

---
+++ src/components/ImageSlider/ImageSliderHolder.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="image-slider">
+        <h2>Image Slider</h2>
+        <ImageSliderCarousel />
+    </div>
+</template>
+
+<script>
+import ImageSliderCarousel from './ImageSliderCarousel';
+import ImageSliderSlide from './ImageSliderSlide'
+
+export default {
+    name: 'ImageSldierHolder',
+    components: {
+        ImageSliderCarousel,
+        ImageSliderSlide
+    }
+}
+</script>
+
+<style scoped>
+.image-slider{
+
+}
+</style>
