2ad96716ef483f87634e234186aaeef93e4bcd35
George Higbie <ghigbie@monigle.com>
2019-02-11 09:04:08 -0700

    Add new vue files

--- src/components/HooperCarousel/HooperCarouselHolder.vue
+++ src/components/HooperCarousel/HooperCarouselHolder.vue
@@ -2,21 +2,43 @@
     <div class="hooper-carousel-holder">
         <h2>Hooper Carousel</h2>
         <p><a href="https://vuejsexamples.com/a-customizable-carousel-slider-optimized-for-vue/">https://vuejsexamples.com/a-customizable-carousel-slider-optimized-for-vue/</a></p>
-        <Hooper>
-            <Slide>
-                <h3>Yo mamma is so stupid...</h3>
-                <p> she thought a pineapple was an apple made out of pine."</p>
-            </Slide>
-            <Slide>
+
+          <hooper :rtl="true">
+    <slide>
+      slide 1
+    </slide>
+    <slide>
+      slide 2
+    </slide>
+    ...
+  </hooper>
+        <Hooper :itemsToShow="3" :centerMode="true" pagination="no">
+
+            <slide>
+                <h2>Yo mamma is so stupid...</h2>
+                <p> she thought a pineapple was an apple made out of a type of wood."</p>
+            </slide>
+
+            <slide>
                 <h3>Yo mamma is so small...</h3>
                 <p> she tried to hang glide on a dorito."</p>
-            </Slide>
-            <Slide>
+            </slide>
+            <slide>
                 <h3>Yo mamma is so ugly...</h3>
                 <p>she thought Jaba the Hut was pretty."</p>
-            </Slide>
+            </slide>
             <Navigation slot="hooper-addons"></Navigation>
         </Hooper>
+
+          <hooper>
+    <slide>
+      slide 1
+    </slide>
+    <slide>
+      slide 2
+    </slide>
+    ...
+  </hooper>
     </div>
 </template>
 
